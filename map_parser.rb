077e0ae8f2a9c702834b036d6e162c57fb3d7376
Nico De Witte <nico.dewitte@vives.be>
2015-10-17 11:00:04 +0200

    Start of map parser

---
+++ map_parser.rb
@@ -0,0 +1,24 @@
+class MapParser
+
+  # [connection]
+  #   instruction door
+  #   room 1
+  # [/connection]
+  def parseConnection(string)
+    
+  end
+
+  # [room]
+  #   id 2
+  #   description The backyard
+  #   [connection]
+  #     instruction window
+  #     room 1
+  #   [/connection]
+  # [/room]
+  def parseRoom(string)
+    
+  end
+
+
+end
