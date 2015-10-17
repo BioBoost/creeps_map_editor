712a1c51fd4efe89f4be6d2f3bb319846a7e6d1d
Nico De Witte <nico.dewitte@vives.be>
2015-10-17 10:58:59 +0200

    Add basic Room and Connection

---
+++ connection.rb
@@ -0,0 +1,7 @@
+class Connection
+  attr_accessor :left, :right, :left_description, :right_description, :left_instruction, :right_instruction
+
+  def to_s
+    "I am a connection"
+  end
+end
---
+++ room.rb
@@ -0,0 +1,7 @@
+class Room
+  attr_accessor :id, :description, :connections
+
+  def to_s
+    "I am a room"
+  end
+end
